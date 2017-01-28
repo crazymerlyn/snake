faf7c0e276227a4ab17bdb2ce67dda4e41de85b8
CrazyMerlyn <crazy.d.merlyn@gmail.com>
2017-01-29 00:57:36 +0530

    Added live score and game over message.

--- src/main.c
+++ src/main.c
@@ -10,8 +10,14 @@ int main(void)
     noecho();
     curs_set(0);
 
+    WINDOW* play_window = newwin(0, 0, 2, 0);
+    WINDOW* score_window = newwin(1, 0, 0, 0);
+    WINDOW* separator_window = newwin(1, 0, 1, 0);
+
+
     int maxx, maxy;
-    getmaxyx(stdscr, maxy, maxx);
+    getmaxyx(play_window, maxy, maxx);
+    
     Field* field = create_field(maxx, maxy);
     grow_snake(field->snake, make_pos(2, 2));
     grow_snake(field->snake, make_pos(2, 3));
@@ -19,6 +25,9 @@ int main(void)
     for (int i = 0; i < 6; ++i) {
         add_food_random(field, FOOD_NORMAL);
     }
+    
+    int highscore = 20;
+    int score = 2;
 
     while (true) {
         int ch = getch();
@@ -40,25 +49,44 @@ int main(void)
         }
 
         move_snake_on_field(field);
+        score = length_snake(field->snake);
 
         if (field->snake_collided == COLLISION_FOOD) {
             add_food_random(field, FOOD_NORMAL);
         } else if (field->snake_collided == COLLISION_SELF) {
             break;
         }
-        erase();
+        werase(score_window);
+        werase(separator_window);
+        werase(play_window);
+
+        wprintw(score_window, "Score: %5d\tHigh Score: %5d", score, highscore);
+        
+        for(int i = 0; i < maxx; ++i) {
+            mvwaddch(separator_window, 0, i, '=');
+        }
 
         for (PosList* it = field->snake->head; it != NULL; it = it->next) {
-            mvaddch(it->y, it->x, '#');
+            mvwaddch(play_window, it->y, it->x, '#');
         }
 
         for (int i = 0; i < field->n_foods; ++i) {
-            mvaddch(field->foods[i].pos.y, field->foods[i].pos.x, ACS_DIAMOND);
+            mvwaddch(play_window, field->foods[i].pos.y, 
+                    field->foods[i].pos.x, ACS_DIAMOND);
         }
+
+        wrefresh(score_window);
+        wrefresh(separator_window);
+        wrefresh(play_window);
     }
     free_field(field);
 
-    timeout(0);
+    timeout(-1);
+    char msg[] = "Game Over. Press any key to exit.";
+    
+    mvwprintw(play_window, maxy*4/5, (maxx - sizeof(msg))/2, msg);
+    wrefresh(play_window);
+
     getch();
     endwin();
     return 0;
